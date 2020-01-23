79028574b0da2eeeed9b8c5efcbf3e54b49b5bed
Nicolas Marlier <nicolas@juliedesk.com>
2020-01-23 10:35:10 +0100

    - Changed key::try_from so it takes a base64-encoded input - Fix EncryptedValueAndId naming into EncryptedAndId - Implemented Key::try_from(String)

--- src/simple_aes256_gcm.rs
+++ src/simple_aes256_gcm.rs
@@ -4,15 +4,21 @@ use std::{fmt, error};
 use std::convert::{TryInto, TryFrom};
 
 #[derive(Debug, Clone)]
-pub struct InvalidKeySizeError;
+pub enum InvalidKeyError {
+    InvalidKeySizeError,
+    InvalidKeyBase64Error
+}
 
-impl fmt::Display for InvalidKeySizeError {
+impl fmt::Display for InvalidKeyError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", "Please provide a 32-byte key")
+        match self {
+            InvalidKeyError::InvalidKeySizeError => write!(f, "{}", "Please provide a 32-byte, base64-encoded, key"),
+            InvalidKeyError::InvalidKeyBase64Error => write!(f, "{}", "Please provide a valid base64"),
+        }
     }
 }
 
-impl error::Error for InvalidKeySizeError {
+impl error::Error for InvalidKeyError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         // Generic error, underlying cause isn't tracked.
         None
@@ -24,15 +30,27 @@ pub struct Key {
 }
 
 impl TryFrom<&str> for Key {
-    type Error = InvalidKeySizeError;
-    fn try_from(key: &str) -> Result<Self, InvalidKeySizeError> {
-        let u8_array: Result<[u8; 32], _> = key.as_bytes().try_into();
+    type Error = InvalidKeyError;
+    fn try_from(base64_key: &str) -> Result<Self, InvalidKeyError> {
+        let key = match base64::decode(base64_key) {
+            Ok(data) => data,
+            Err(_) => return Err(InvalidKeyError::InvalidKeyBase64Error)
+        };
+
+        let u8_array: Result<[u8; 32], _> = key.as_slice().try_into();
         match u8_array {
             Ok(value) => Ok(Self {
                 u8_array: value
             }),
-            Err(_) => Err(InvalidKeySizeError)
+            Err(_) => Err(InvalidKeyError::InvalidKeySizeError)
         }
+    }
+}
+
+impl TryFrom<String> for Key {
+    type Error = InvalidKeyError;
+    fn try_from(base64_key: String) -> Result<Self, InvalidKeyError> {
+        Self::try_from(&base64_key[..])
     }
 }
 
@@ -66,12 +84,12 @@ pub struct Iv {
 impl TryFrom<&str> for Iv {
     type Error = InvalidIvError;
     fn try_from(base64_iv: &str) -> Result<Iv, InvalidIvError> {
-        let encrypted_value = match base64::decode(base64_iv) {
+        let iv = match base64::decode(base64_iv) {
             Ok(data) => data,
             Err(_) => return Err(InvalidIvError::InvalidIvBase64Error)
         };
 
-        let u8_array: Result<[u8; 12], _> = encrypted_value.as_slice().try_into();
+        let u8_array: Result<[u8; 12], _> = iv.as_slice().try_into();
         match u8_array {
             Ok(value) => Ok(Iv {
                 u8_array: value
@@ -128,17 +146,17 @@ impl<'a> fmt::Display for Decrypted<'a> {
 }
 
 
-pub struct EncryptedValueAndId {
+pub struct EncryptedAndIv {
     pub encrypted: Encrypted,
     pub iv: Iv
 }
 
-pub fn encrypt<'a>(key: &Key, decrypted: &Decrypted) -> Result<EncryptedValueAndId, aead::Error> {
+pub fn encrypt<'a>(key: &Key, decrypted: &Decrypted) -> Result<EncryptedAndIv, aead::Error> {
     let iv = Iv::generate();
     let nonce = GenericArray::from_slice(&iv.u8_array);
     let client = Aes256Gcm::new(GenericArray::clone_from_slice(&key.u8_array));
     let ciphertext =  client.encrypt(nonce, decrypted.value.as_bytes())?;
-    Ok(EncryptedValueAndId {
+    Ok(EncryptedAndIv {
         iv: iv,
         encrypted: Encrypted {
             u8_vec: ciphertext
@@ -167,11 +185,11 @@ impl error::Error for DecryptionError {
     }
 }
 
-pub fn decrypt(key: &Key, encrypted_value_and_iv: EncryptedValueAndId) -> Result<String, DecryptionError> {
-    let nonce = GenericArray::from_slice(&encrypted_value_and_iv.iv.u8_array);
+pub fn decrypt(key: &Key, encrypted_and_iv: EncryptedAndIv) -> Result<String, DecryptionError> {
+    let nonce = GenericArray::from_slice(&encrypted_and_iv.iv.u8_array);
     let client = Aes256Gcm::new(GenericArray::clone_from_slice(&key.u8_array));
 
-    match client.decrypt(nonce, encrypted_value_and_iv.encrypted.u8_vec.as_ref()) {
+    match client.decrypt(nonce, encrypted_and_iv.encrypted.u8_vec.as_ref()) {
         Ok(decrypted_u8_vec) => match String::from_utf8(decrypted_u8_vec) {
             Ok(decrypted_string) => Ok(decrypted_string),
             Err(_) => Err(DecryptionError::InvalidUTF8DecryptionError)
@@ -184,24 +202,41 @@ pub fn decrypt(key: &Key, encrypted_value_and_iv: EncryptedValueAndId) -> Result
 mod tests {
     use super::*;
     #[test]
-    fn key_new_3_bytes_fails() {
+    fn key_try_from_invalid_base64_fails() {
         match Key::try_from("012") {
             Ok(_) => assert!(false),
-            Err(_) => assert!(true)
+            Err(e) => match e {
+                InvalidKeyError::InvalidKeySizeError => assert!(false, "Should err an InvalidKeyError::InvalidKeyBase64Error"),
+                InvalidKeyError::InvalidKeyBase64Error => assert!(true)
+            }
         }
     }
 
     #[test]
-    fn key_new_33_bytes_fails() {
-        match Key::try_from("012345678901234567890123456789012") {
+    fn key_try_from_valid_3byte_fails() {
+        match Key::try_from("MDEy") {
             Ok(_) => assert!(false),
-            Err(_) => assert!(true)
+            Err(e) => match e {
+                InvalidKeyError::InvalidKeyBase64Error => assert!(false, "Should err an InvalidKeyError::InvalidKeySizeError"),
+                InvalidKeyError::InvalidKeySizeError => assert!(true)
+            }
         }
     }
 
     #[test]
-    fn key_new_32_bytes_succeeds() {
-        match Key::try_from("01234567890123456789012345678901") {
+    fn key_try_from_valid_33byte_fails() {
+        match Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDEy") {
+            Ok(_) => assert!(false),
+            Err(e) => match e {
+                InvalidKeyError::InvalidKeyBase64Error => assert!(false, "Should err an InvalidKeyError::InvalidKeySizeError"),
+                InvalidKeyError::InvalidKeySizeError => assert!(true)
+            }
+        }
+    }
+
+    #[test]
+    fn key_try_from_valid_32_bytes_succeeds() {
+        match Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=") {
             Err(_) => assert!(false, "Should succeed"),
             Ok(key) => assert_eq!(key.u8_array, [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49])
         }
@@ -302,11 +337,11 @@ mod tests {
     #[test]
     fn encrypted_values_are_different_for_same_inputs() {
         let encrypted_1 = encrypt(
-            &Key::try_from("12345678901234567890123456789012").unwrap(),
+            &Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=").unwrap(),
             &Decrypted::from("This is a text.")
         ).unwrap();
         let encrypted_2 = encrypt(
-            &Key::try_from("12345678901234567890123456789012").unwrap(),
+            &Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=").unwrap(),
             &Decrypted::from("This is a text.")
         ).unwrap();
         assert!(encrypted_1.encrypted.u8_vec != encrypted_2.encrypted.u8_vec)
@@ -315,11 +350,11 @@ mod tests {
     #[test]
     fn encrypted_values_are_different_for_different_inputs() {
         let encrypted_1 = encrypt(
-            &Key::try_from("12345678901234567890123456789012").unwrap(),
+            &Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=").unwrap(),
             &Decrypted::from("This is a text.")
         ).unwrap();
         let encrypted_2 = encrypt(
-            &Key::try_from("12345678901234567890123456789012").unwrap(),
+            &Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=").unwrap(),
             &Decrypted::from("This is another text.")
         ).unwrap();
         assert!(encrypted_1.encrypted.u8_vec != encrypted_2.encrypted.u8_vec)
@@ -327,7 +362,7 @@ mod tests {
 
     #[test]
     fn encrypt_decrypt_is_iso() {
-        let key = Key::try_from("12345678901234567890123456789012").unwrap();
+        let key = Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=").unwrap();
         let encrypted = encrypt(
             &key,
             &Decrypted::from("This is a text.")
@@ -337,9 +372,19 @@ mod tests {
     }
 
     #[test]
-    // TO WRITE
+    fn encrypt_decrypt_is_iso_with_string_key() {
+        let key = Key::try_from(String::from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=")).unwrap();
+        let encrypted = encrypt(
+            &key,
+            &Decrypted::from("This is a text.")
+        ).unwrap();
+
+        assert_eq!(decrypt(&key, encrypted).unwrap(), String::from("This is a text."))
+    }
+
+    #[test]
     fn decrypt_fails_when_non_utf8() {
-        let key = Key::try_from("12345678901234567890123456789012").unwrap();
+        let key = Key::try_from("MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=").unwrap();
 
         let iv = Iv::generate();
         let nonce = GenericArray::from_slice(&iv.u8_array);
@@ -347,14 +392,14 @@ mod tests {
         let invalid_utf8_bytes: &[u8] = &[133u8, 133u8];
         let ciphertext = client.encrypt(nonce, invalid_utf8_bytes).unwrap();
 
-        let encrypted_value_and_iv = EncryptedValueAndId {
+        let encrypted_and_iv = EncryptedAndIv {
             iv: iv,
             encrypted: Encrypted {
                 u8_vec: ciphertext
             }
         };
 
-        match decrypt(&key, encrypted_value_and_iv) {
+        match decrypt(&key, encrypted_and_iv) {
             Ok(_) => assert!(false, "Should err InvalidUTF8DecryptionError"),
             Err(e) => match e {
                 DecryptionError::GenericDecryptionError => assert!(false, "Should err InvalidUTF8DecryptionError"),
@@ -367,11 +412,11 @@ mod tests {
     // Not able to find any example that would make this err...
     // fn decrypt_err_when_decryption_error() {
     //     let key = Key::try_from("12345678901234567890123456789012").unwrap();
-    //     let encrypted_value_and_iv = EncryptedValueAndIv {
+    //     let encrypted_and_iv = EncryptedAndIv {
     //         encrypted: encrypted,
     //         iv: iv
     //     }
-    //     match decrypt(&key, encrypted_value_and_iv) {
+    //     match decrypt(&key, encrypted_and_iv) {
     //         Ok(_) => assert!(false, "Should err GenericDecryptionError"),
     //         Err(e) => match e {
     //             DecryptionError::InvalidUTF8DecryptionError => assert!(false, GenericDecryptionError),
